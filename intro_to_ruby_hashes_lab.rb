b66e33bc041c492b7bad82a37dfd21e8a820b818
Sean Fyffe <stfyffe@gmail.com>
2019-10-01 20:46:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,12 +13,12 @@ def base_hash
 			:names=>
 			{
 				:reading_railroad=>
+				:pennsylvania_railroad=>
+				:b_and_o_railroad
+				:shortline
 			}
 		}
 	}
-
-
-
 	# Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	{}
